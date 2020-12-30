273b65bccaf5111bd8d5f6a11ab49f6e50838bd0
Sarawoot Chaichana <sarawoot_202@hotmail.com>
2020-12-30 17:37:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "KB32-FT",
   vendor: "MakerAsia",
   platform: "arduino-esp32",
-  title: "KB32-FT",
+  title: "KB32-FT (Arduino API)",
   description: "KB32-FT Compatible Kidbright32",
   author: "Maker Asia",
   website: "http://www.kidbright.club",
